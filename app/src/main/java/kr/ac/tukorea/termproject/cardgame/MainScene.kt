4fb5b8b5334c5522ab8192462207dcc9d97f2caf
RcoasteR0 <92726320+RcoasteR0@users.noreply.github.com>
2026-05-21 18:22:34 +0900

    기본 프레임워크

---
+++ app/src/main/java/kr/ac/tukorea/termproject/cardgame/MainScene.kt
@@ -0,0 +1,15 @@
+package kr.ac.tukorea.termproject.cardgame
+
+import kr.ac.tukorea.ge.spgp2026.a2dg.scene.Scene
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+
+class MainScene(gctx: GameContext) : Scene(gctx) {
+    override fun onEnter() {
+        // Initialize your game objects here
+    }
+
+    override fun update(gctx: GameContext) {
+        super.update(gctx)
+        // Add scene-specific update logic
+    }
+}
